f8a2de95261f67075ba9dd6fa1367b1b3e78ab22
StefanoPalazzo <s.palazzo@alumno.um.edu.ar>
2023-08-17 21:26:26 -0300

    Added loop to generate tiles with values

--- game/game_models.py
+++ game/game_models.py
@@ -1,43 +1,48 @@
 import unittest, random
 
+Tile_Count_Value= {
+    'A': [12, 1],
+    'E': [12, 1],
+    'I': [6, 1],
+    'L': [4, 1],
+    'N': [5, 1],
+    'O': [9, 1],
+    'R': [5, 1],
+    'S': [6, 1],
+    'T': [4, 1],
+    'U': [5, 1],
+    'D': [5, 2],
+    'G': [2, 2],
+    'B': [2, 3],
+    'C': [4, 3],
+    'M': [2, 3],
+    'P': [2, 3],
+    'F': [1, 4],
+    'H': [2, 4],
+    'V': [1, 4],
+    'Y': [1, 4],
+    'CH': [1, 5],
+    'Q': [1, 5],
+    'J': [1, 8],
+    'LL': [1, 8],
+    'Ñ': [1, 8],
+    'RR': [1, 8],
+    'X': [1, 8],
+    'Z': [1, 10],
+}
+
+
 class Tile:
     def __init__(self,letter,value):
         self.letter = letter
         self.value = value
 
-
 class BagTiles():
     def __init__(self):
-        self.tiles = [
-            Tile('A',1),
-            Tile('E',1),
-            Tile('I',1),
-            Tile('O',1),
-            Tile('U',1),
-            Tile('L',1),
-            Tile('N',1),
-            Tile('S',1),
-            Tile('T',1),
-            Tile('R',1),
-            Tile('D',2),
-            Tile('G',2),
-            Tile('B',3),
-            Tile('C',3),
-            Tile('M',3),
-            Tile('P',3),
-            Tile('F',4),
-            Tile('H',4),
-            Tile('V',4),
-            Tile('W',4),
-            Tile('Y',4),
-            Tile('K',5),
-            Tile('Q',5),
-            Tile('J',8),
-            Tile('X',8),
-            Tile('Ñ',8),
-            Tile('Z',10),
-            Tile('', 0),
-        ]
+        self.tiles = []
+        for i in Tile_Count_Value:    #letter of the tile
+            for j in range(Tile_Count_Value[i][0]):    #Amount of tiles of that letter
+                self.tiles.append(Tile(i,  Tile_Count_Value[i][1])) # Adds a tile, i for the letter, and Tile_Count_Value[i][1] for the value
         random.shuffle(self.tiles)
 
     def take(self, amount):
@@ -49,6 +54,10 @@ class BagTiles():
     def put(self, tiles):
         self.tiles.extend(tiles)
 
+
+# # Prints each Tile and its value
+# for i in range(len(BagTiles().tiles)):         
+#       print (BagTiles().tiles[i].letter,BagTiles().tiles[i].value)
 
 
 
