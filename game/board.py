7991e255e4f3e4811d08926d7db4dc197f0090b0
StefanoPalazzo <s.palazzo@alumno.um.edu.ar>
2023-08-31 16:48:47 -0300

    [0.0.8] Calculate word value function

--- game/board.py
+++ game/board.py
@@ -1,10 +1,62 @@
 from game.cell import Cell
-
+from game.models import Tile
 
 class Board:
     def __init__(self):
         self.grid = [
-            [ Cell(1, '') for _ in range(15) ]
+            [ Cell(None,1, None) for _ in range(15) ]
             for _ in range(15)
         ]
 
+# a = [
+#           [ '  ' for _ in range(15) ]
+#             for _ in range(15)
+#         ]
+
+# def rotate(mat):
+#     N = len(mat)
+    
+#     # Transponer la matriz
+#     for i in range(N):
+#         for j in range(i + 1, N):
+#             mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+    
+#     # Invertir las filas
+#     for i in range(N):
+#         mat[i] = mat[i][::-1]
+
+
+# for i in range(4):
+#     for i in range(7):
+#      for j in range (7):
+#         if i == 0:
+#             a[i][i] = 'TP'
+#         elif i == 5:
+#             a[i][i] = 'TL'
+#         elif i == 6:
+#             a[i][i] = 'DL'
+#         else:
+#          a[i][i] = 'DP'
+ 
+#     rotate(a)
+# a[7][7] = ' X'
+
+# for i in  range(15):
+#      print (a[i])
+
+
+
+
+
+def calculate_word_value(word):
+        word_value = 0
+        word_multiplier = 1
+        for i in word:
+            word_value += i.calculate_value()
+            if i.multiplier_type == 'word':
+                word_multiplier = i.multiplier
+                i.multiplier_type = None         # Deactivates the multiplier of the cell
+        word_value *= word_multiplier 
+        return(word_value)
+        
+        
---
+++ tests/test_calculate_word_value.py
@@ -0,0 +1,84 @@
+import unittest
+from game.cell import Cell
+from game.models import Tile
+from game.board import calculate_word_value
+
+
+class TestCalculateWordValue(unittest.TestCase):
+    def test_simple(self):
+        word = [
+            Cell(letter=Tile('C', 1)),
+            Cell(letter=Tile('A', 1)),
+            Cell(letter=Tile('S', 2)),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calculate_word_value(word)
+        self.assertEqual(value, 5)
+
+    def test_with_letter_multiplier(self):
+        word = [
+            Cell(letter=Tile('C', 1)),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='letter',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calculate_word_value(word)
+        self.assertEqual(value, 7)
+
+    def test_with_word_multiplier(self):
+        word = [
+            Cell(letter=Tile('C', 1)),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='word',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calculate_word_value(word)
+        self.assertEqual(value, 10)
+
+    def test_with_letter_word_multiplier(self):
+        word = [
+            Cell(
+                multiplier=3,
+                multiplier_type='letter',
+                letter=Tile('C', 1)
+            ),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='word',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calculate_word_value(word)
+        self.assertEqual(value, 14)
+
+    def test_with_letter_word_multiplier_no_active(self):
+        word = [
+            Cell(
+                multiplier=3,
+                multiplier_type='letter',
+                letter=Tile('C', 1)
+            ),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='word',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value0 = calculate_word_value(word)    # Calculates word value with multipliers
+        value1 = calculate_word_value(word)    # Calculates word value without multipliers (multipliers deactivated)
+        self.assertEqual(value1, 5)
+
+if __name__ == '__main__':
+    unittest.main()
